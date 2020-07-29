adebe83e9c1cac1a26a9e5d27d53e4b9f22e9ff8
AvrilSf <tusowalij@gmail.com>
2020-07-29 15:04:38 -0600

    change description

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,26 @@
 # Both of the functions below are strongly based in the example provided in the
-# assignment page, as I am really new in programming, I am trying to learn by
-# applying all the examples. I'm sure that as I keep learning, I will be able to 
-# be creative in the ways of writing code. If this is not what is expected, 
-# please give suggestions to improve. Please help me keep learning.
+# assignment page.
 
 # The first function, makeCacheMatrix creates a square "matrix".
 # The output is a list containing a function to set and get the value of the
 # square matrix and also, set and get the value of its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-        mtr <- NULL
-        set <- function(y) {
-                x <<- y
-                mtr <<- NULL
+        mtr <- NULL               ## Initialize mtr as NULL; will hold value of
+                                  ## matrix inverse.
+        set <- function(y) {      ## Define the set function to assign new.
+                x <<- y           ## Value of matrix in parent environment.
+                mtr <<- NULL      ## If there is a new matrix,reset mtr to NULL.
         }
-        get <- function() x
-        set_inve <- function(solve) mtr <<- solve
-        get_inve <- function() mtr
+        get <- function() x       ## Define the get function - returns value of
+                                  ## the matrix argument.        
+        set_inve <- function(solve) mtr <<- solve    ## Assigns value of mtr in
+                                  ## parent environment.
+        get_inve <- function() mtr ## Gets the value of mtr where called.
         list(set = set, get = get,
              set_inve = set_inve,
-             get_inve = get_inve)
+             get_inve = get_inve)  ## You need this in order to refer to the 
+                                   ## functions with the $ operator.
 }
 
 
@@ -32,13 +33,13 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         mtr <- x[get_inve()]
-        if(!is.null(mtr)) {
-                message("getting cached data")
-                return(mtr)
-        }
-        data2 <- x$get()
-        mtr <- solve(data2, 1)
-        x$set_inve(mtr)
-        mtr
+        if(!is.null(mtr)) {                     # In inverse matrix is not NULL
+                message("getting cached data")  # type message:"getting cached..
+                return(mtr)                     # return the invertible matrix
+        }                       # if value of invertible matrix is null then
+        data2 <- x$get()        # get the original matrix data 
+        mtr <- solve(data2, 1)  # use solve function to inverse matrix
+        x$set_inve(mtr)         # set the invertible matrix
+        mtr                     # return the invertible matrix
         
 }
